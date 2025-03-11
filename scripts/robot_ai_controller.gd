d7f789ee48b413a635b00e9fabd08a1f3cfeaac7
Krajot <jdavidortiz2002@gmail.com>
2025-03-11 09:08:03 +0100

    Updating robot AI controller script and the reward functions

--- scripts/robot_ai_controller.gd
+++ scripts/robot_ai_controller.gd
@@ -18,11 +18,66 @@ var min_dist: float
 
 
 func get_obs() -> Dictionary:
-	return {}
+	var observations: Array[float] = []
+	for raycast_sensor in raycast_sensors:
+		observations.append_array(raycast_sensor.get_observation())
+
+	var level_size = 16.0
+
+	var chest_local = to_local(chest.global_position)
+	var chest_direction = chest_local.normalized()
+	var chest_distance = clampf(chest_local.length(), 0.0, level_size)
+	
+	var lever_local = to_local(lever.global_position)
+	var lever_direction = lever_local.normalized()
+	var lever_distance = clampf(lever_local.length(), 0.0, level_size)
+		
+	var key_local = to_local(key.global_position)
+	var key_direction = key_local.normalized()
+	var key_distance = clampf(key_local.length(), 0.0, level_size)
+	
+	var raft_local = to_local(raft.global_position)
+	var raft_direction = raft_local.normalized()
+	var raft_distance = clampf(raft_local.length(), 0.0, level_size)
+	
+	var player_speed = player.global_basis.inverse() * player.velocity.limit_length(5.0) / 5.0
+
+	(
+		observations
+		.append_array(
+			[
+				chest_direction.x,
+				chest_direction.y,
+				chest_direction.z,
+				chest_distance,
+				lever_direction.x,
+				lever_direction.y,
+				lever_direction.z,
+				lever_distance,
+				key_direction.x,
+				key_direction.y,
+				key_direction.z,
+				key_distance,
+				raft_direction.x,
+				raft_direction.y,
+				raft_direction.z,
+				raft_distance,
+				raft.movement_direction_multiplier,
+				float(player._is_lever_pulled),
+				float(player._is_chest_opened),
+				float(player._is_key_collected),
+				float(player.is_on_floor()),
+				player_speed.x,
+				player_speed.y,
+				player_speed.z,
+			]
+		)
+	)
+	return {"obs": observations}
 
 
 func get_reward() -> float:
-	return 0
+	return reward
 
 
 func _physics_process(delta: float) -> void:
